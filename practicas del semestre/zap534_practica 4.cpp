0a708009050058bd1d6699e679e3431efe6b159d
vamprock19 <94476879+vamprock19@users.noreply.github.com>
2021-12-03 16:44:15 -0600

    Add files via upload

---
+++ practicas del semestre/zap534_practica 4.cpp
@@ -0,0 +1,22 @@
+// zap534_practica 4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+
+// ordenar double double at Tim Horton
+
+int numero_entero = 120;
+float numero_flotante = 3.25f;
+char cafes_disponibles;
+double doble_variable_STD = 375.9870f;
+string esta_es_una_string = "cafe double double";
+int main()
+{
+    printf("menu del dia"); cout << endl; // normal
+    printf("%s", esta_es_una_string.c_str()); cout << endl; // string
+    printf("el costo por 25 double doubles %d", numero_entero); cout << endl;// int
+    printf("precio por un double double %f", numero_flotante); cout << endl; //float
+    printf("precio por 580 double doubles %1f", doble_variable_STD); cout << endl;//double
+}
